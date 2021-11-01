9b636815254e6b0bcd565379769ec9c37797b544
Spencer Siu <srsiu@uci.edu>
2021-10-31 23:12:34 -0700

    fixed repository error

--- src/main/java/edu/sjsu/cmpe275/lab2/Controller/PassengerController.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/Controller/PassengerController.java
@@ -3,7 +3,6 @@ package edu.sjsu.cmpe275.lab2.Controller;
 import edu.sjsu.cmpe275.lab2.DataSource.PassengerRepository;
 import edu.sjsu.cmpe275.lab2.Model.Passenger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("passenger")
 @RestController
 public class PassengerController {
-    private final PassengerRepository passengerRepository;
+    private PassengerRepository passengerRepository;
+
     @Autowired
-    public PassengerController(@Qualifier("passengerRepository") PassengerRepository passengerRepository) {
+    public PassengerController(PassengerRepository passengerRepository) {
         this.passengerRepository = passengerRepository;
     }
 
--- src/main/java/edu/sjsu/cmpe275/lab2/Lab2Application.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/Lab2Application.java
@@ -2,8 +2,10 @@ package edu.sjsu.cmpe275.lab2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("edu...lab2")
 public class Lab2Application {
 
     public static void main(String[] args) {
